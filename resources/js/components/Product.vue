982f92040914b29451486b883a21a16b78927876
“SamTurnover” <samir@turnoverbnb.com>
2023-07-03 23:08:42 -0300

    Implementing a Home page with fake data

---
+++ resources/js/components/Product.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="card">
+        <div class="card-header">{{ value.name }}</div>
+
+        <div class="card-body">
+            {{ value.description }}
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+    name: 'Product',
+        props: {
+            value: {
+                type: Object,
+            },
+        },
+    }
+</script>
---
+++ resources/views/Home.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+        <h3>Products</h3>
+        <div style="margin-bottom: 5px;" v-for="(product, index) in products" :key="index">
+            <product v-model="products[index]"/>
+        </div>
+    </div>
+</template>
+
+<script>
+import Product from '../js/components/Product.vue';
+export default {
+        components: {
+            Product,
+        },
+        name: 'Home',
+        data() {
+            return {
+                products: [
+                    {
+                        name: 'Mouse',
+                        description: 'Mucho texto'
+                    },
+                    {
+                        name: 'Keyboard',
+                        description: 'Mucho texto'
+                    },
+                    {
+                        name: 'Monitor',
+                        description: 'Mucho texto'
+                    },
+                    {
+                        name: 'Pc gamer',
+                        description: 'Mucho texto'
+                    },
+                ]
+            }
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
